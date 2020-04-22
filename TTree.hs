29e6e9e57f4a4ade37827944d69f876227bf222e
BMarelli <marellibautista@gmail.com>
2020-04-22 17:43:09 -0300

    keys terminado

--- TTree.hs
+++ TTree.hs
@@ -7,7 +7,7 @@ module TP1 where
 data TTree k v = Node k (Maybe v) (TTree k v) (TTree k v) (TTree k v)
                 |Leaf k v
                 |E
-                deriving Show
+                deriving (Show, Eq)
 
 -- t = Node 'r' Nothing E (Node 'e' (Just 16) (Node 'a' Nothing E (Leaf 's' 1) E) (Node 'o' (Just 2) (Leaf 'd' 9) E (Leaf 's' 4)) E) (Node 's' Nothing E (Node 'i' (Just 4) (Leaf 'e' 8) (Leaf 'n' 7) E) E)
 
@@ -49,33 +49,34 @@ y = insert "cosc" 6 t
 u = insert "cosd" 7 y
 i = insert "cosde" 8 u
 
--- mergeMaximoHijo :: TTree k v -> TTree k v
--- mergeMaximoHijo nodo@(Node k v xs ys zs) = case ys of
---                                             E -> Node k v xs (maximoHijo xs) zs
---                                             otherwise -> node
+mergeMaximoHijo :: TTree k v -> TTree k v
+mergeMaximoHijo nodo@(Node k v xs ys zs) = if ys == E then (Node k v xs (maximoHijo xs) zs)
+                                                      else (Node k v xs ys zs)
+    where maximoHijo xs@(Node _ _ _ _ zs) = case zs of
+                                                E -> xs
+                                                Leaf _ _ -> zs
+                                                otherwise -> maximoHijo zs
 
---     where maximoHijo xs@(Node _ _ _ _ zs) = case zs of
---                                                 E -> xs
---                                                 Leaf _ _ -> zs
---                                                 otherwise -> maximoHijo zs
-
--- delete :: Ord k => [k] -> TTree k v -> TTree k v
--- delete _ E = E
--- delete (c:cs) (Leaf k v) | c == k    = case cs of
---                                         [] -> E
---                                         otherwise -> Leaf k v
---                          | otherwise = Leaf k v
--- -- delete [c] (Node k v xs ys zs) 
--- delete a@(c:cs) (Node k v xs ys zs) | c < k       = Node k v (delete a xs) ys zs
---                                     | c > k       = Node k v xs ys (delete a zs)
---                                     | otherwise   = case cs of                   
---                                                         [] -> case zs of
---                                                                 E -> Node k Nothing xs ys zs
---                                                                 Node k v xs ys zs -> 
---                                                         otherwise -> delete cs ys                                                       
+delete :: Ord k => [k] -> TTree k v -> TTree k v
+delete _ E = E
+delete (c:cs) (Leaf k v) | c == k    = case cs of
+                                        [] -> E
+                                        otherwise -> Leaf k v
+                         | otherwise = Leaf k v
+-- delete [c] (Node k v xs ys zs) 
+delete a@(c:cs) (Node k v xs ys zs) | c < k       = Node k v (delete a xs) ys zs
+                                    | c > k       = Node k v xs ys (delete a zs)
+                                    | otherwise   = case cs of                   
+                                                        [] -> case zs of
+                                                                E -> Node k Nothing xs ys zs
+                                                                Node k v xs ys zs -> 
+                                                        otherwise -> delete cs ys
 
 --Dado un arbol devuelve una lista ordenada con las claves del mismo.
 keys :: TTree k v -> [[k]]
-keys E = []
-keys (Leaf k v) = [[k]]
-keys (Node k v xs ys zs) = (keys xs) ++ ([[k]] ++ (keys ys)) ++ (keys zs)
+keys xs = keys_ xs []
+ where keys_ E _ = []
+       keys_ (Leaf k v) cs           = [cs ++ [k]]
+       keys_ (Node k v xs ys zs) cs  = case v of
+                                       Just _ -> (keys_ xs cs) ++ [cs ++ [k]] ++ (keys_ ys (cs ++ [k])) ++ (keys_ zs cs)
+                                       otherwise -> (keys_ xs cs) ++ (keys_ ys (cs ++ [k])) ++ (keys_ zs cs)
